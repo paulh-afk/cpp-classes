701ab664d6294e3eeb43628d2e77060673fac7dd
Paul <paul.helleu@orange.fr>
2022-03-01 22:14:57 +0100

    guerrier

---
+++ Headers/Guerrier.hpp
@@ -0,0 +1,12 @@
+#if !defined(GUERRIER)
+#define GUERRIER
+
+#include <iostream>
+#include <string>
+#include "../Headers/Arme.hpp"
+
+class Guerrier : public Personnage {
+
+};
+
+#endif // GUERRIER
